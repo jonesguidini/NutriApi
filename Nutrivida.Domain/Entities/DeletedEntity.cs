5ab224ab7f8236d0582ac9c21132c09be1d28bbb
Jones Guidini <jonesguidini@gmail.com>
2020-02-18 15:54:27 -0300

    feat: adição da entidade DeletedEntity
    Esta entidade é responsável por armazenar na tabela a referencia da exclusão do registro

---
+++ Nutrivida.Domain/Entities/DeletedEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nutrivida.Domain.Entities
+{
+    public abstract class DeletedEntity : BaseEntity
+    {
+        public bool IsDeleted { get; set; }
+        public DateTime? DateDelited { get; set; }
+        public int? DeletedByUserId { get; set; }
+        public User? DeletedByUser { get; set; }
+    }
+}
